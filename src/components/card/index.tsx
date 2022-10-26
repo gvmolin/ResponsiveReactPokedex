43ac3b0901e420d4535ceeea959efd920395328f
gvmolin <molinguin@gmail.com>
2022-10-25 22:30:36 -0300

    Connection with API, list render, styles, etc

---
+++ src/components/card/index.tsx
@@ -0,0 +1,59 @@
+import findColorByType from "../../utils/style/typeColors";
+import capitalizeFLetter from "../../utils/tools/string";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import style from "./style.module.scss";
+
+interface IData {
+  data:{
+    name:string,
+    url:string,
+  }
+}
+
+export default function Card (props:IData) {
+  const [pokemon, setPokemon] = useState({
+    id:0,
+    name:"",
+    sprites:{
+      front_default:""
+    },
+    types:[{
+      type: {
+        name:""
+      }
+    }]
+  });
+  const [color, setColor] = useState({
+    name:"",
+    color:"",
+  });
+
+  async function getPkmn() {
+    await axios.get(props.data.url)
+      .then(res => {
+        setPokemon(res.data);
+        return res.data;
+      })
+      .then(res => {
+        const findColor = findColorByType(res.types[0].type.name);
+        setColor(findColor);
+      });
+  }
+
+  useEffect(()=>{
+    getPkmn();
+  },[]);
+
+  return (
+    <div className={style.card} style={{backgroundColor: color?.color}}>
+      <div className={style.imageContainer}>
+        <img src={pokemon.sprites.front_default} alt="Pokemon image" />
+      </div>
+      <div className={style.textContainer}>
+        <h3>#{pokemon.id}</h3>
+        <h2>{capitalizeFLetter(pokemon.name)}</h2>
+      </div>
+    </div>
+  );
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import Card from "../../components/card";
+import style from "./style.module.scss";
+import axios from "axios";
+
 export default function Home (){
+  const [pokemons, setPokemons] = useState([]);
+
+  async function getPokemonList(){
+    await axios.get("https://pokeapi.co/api/v2/pokemon/").then(res => {
+      setPokemons(res.data.results);
+    }).catch(err => {
+      console.log(err);
+    });
+  }
+
+  useEffect(() => {
+    getPokemonList();
+  }, []);
+
   return (
-    <h1>home</h1>
+    <div className={style.content}>
+      <div className={style.list}>
+        {pokemons.map((pokemon,i) => (
+          <Card key={i} data={pokemon} />
+        ))}
+      </div>
+    </div>
   );
 }
